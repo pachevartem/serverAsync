4921d0a4cccab9bc18add274f3517c61a83701fa
Pachev Artem <pachevartem@ya.ru>
2018-09-09 21:52:13 +0300

    two

--- serverapp/Program.cs
+++ serverapp/Program.cs
@@ -31,12 +31,12 @@ public class AsynchronousSocketListener
         // Establish the local endpoint for the socket.  
         // The DNS name of the computer  
         // running the listener is "host.contoso.com".  
-        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-        IPAddress ipAddress = ipHostInfo.AddressList[0];
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        IPHostEntry ipHostInfo = Dns.GetHostEntry("188.225.82.212");
+        //IPAddress ipAddress = Dns.GetHostEntry("188.225.82.212");
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("188.225.82.212"), 11000);
 
         // Create a TCP/IP socket.  
-        Socket listener = new Socket(ipAddress.AddressFamily,
+        Socket listener = new Socket(IPAddress.Parse("188.225.82.212").AddressFamily,
             SocketType.Stream, ProtocolType.Tcp);
 
         // Bind the socket to the local endpoint and listen for incoming connections.  
